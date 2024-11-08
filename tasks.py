0bcf6de665bf82e57c231d1a9cf7f8b084328dfe
mlazar1337 <lazarmarek01@gmail.com>
2024-11-08 16:52:07 +0100

    commit

--- tasks.py
+++ tasks.py
@@ -18,14 +18,17 @@ from sklearn import datasets
 # Copy and paste the code for that function here:
 # -----------------------------------------------
 def my_mlp(w, X, sigma=np.tanh):
-    W1 = np.array(w[0:4*6]).reshape(4,6)
-    W2 = np.array(w[4*6:7*4+6*4]).reshape(7,4)
-    W3 = np.array(w[7*4+6*4:]).reshape(1,7)
-   
+    
+    # Layer connection
+    W1 = np.array(w[0:(4*6)].reshape(4,6))
+    W2 = np.array(w[(4*6):(7*4)+6*4 ].reshape(7,4))
+    W3 = np.array(w[(7*4)+6*4:7+4*6+7*4].reshape(1,7))
+    
+    # Implementing equations
     a1 = sigma(W1 @ X)
     a2 = sigma(W2 @ a1)
-    f  = sigma(W3 @ a2)
-    return f
+    f = sigma(W3 @ a2)
+    return f 
 # -----------------------------------------------
  
 # Task 2:
@@ -40,7 +43,7 @@ def my_mlp(w, X, sigma=np.tanh):
 # -----------------------------------------------
 def MSE_func(w, X, y): # give the appropriate name and arguments
     f = my_mlp(w, X)
-    MSE = np.sum((y-f)**2)
+    MSE = np.sum((f - y)**2)
     return MSE
 # -----------------------------------------------
  
@@ -57,8 +60,7 @@ def MSE_func(w, X, y): # give the appropriate name and arguments
 # Copy and paste the code for that function here:
 # -----------------------------------------------
 def dR(beta, x, y):
-    dbeta_0 = 2*np.mean((beta[0] + beta[1]*x - y))   # implement the above formula for dR/dβ₀
-    dbeta_1 = 2*np.mean((beta[0] + beta[1]*x - y)*x) # implement the above formula for dR/dβ₁
-    return np.array([dbeta_0, dbeta_1])
- 
+    dRdb0 = 2*np.mean(beta[0]+beta[1]*x - y)
+    dRdb1 = 2*np.mean((beta[0]+beta[1]*x - y)*x)
+    return np.array([dRdb0, dRdb1])
 # -----------------------------------------------
